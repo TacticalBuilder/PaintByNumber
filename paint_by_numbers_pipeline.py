786d4f0f61288579c7bb02ad81bd0995f0f5d035
Kyle <kylebuettner0@gmail.com>
2021-04-08 20:50:26 -0400

    simpler test images

--- paint_by_numbers_pipeline.py
+++ paint_by_numbers_pipeline.py
@@ -8,20 +8,22 @@ import time
 import cv2
 
 # Options to Run Pipeline
-image_name = 'baseball_field.jpeg'			# Name of Image
+image_name = 'test_images/carrot.jpg'			# Name of Image
 reshape_image = True						# Whether to reshape image dimensions
-reshape_width = 500
-reshape_height = 300
+reshape_width = 250
+reshape_height = 250
 color_code = 1 								# Color code to read in (0 = grayscale, 1 = BGR)
-num_colors = 5								# Number of colors needed for k-means clustering
+num_colors = 3								# Number of colors needed for k-means clustering
 median_kernel = 5							# Size of median kernel used for blurring
 min_canny = 100								# Thresholds used for Canny edge detection
 max_canny = 200
 perimeter_or_area_contour = 'perimeter'		# How to filter out contours
 contour_threshold = 50						# Threshold to filter contours	
-filter_verbose = False						# Print contour info				
+filter_verbose = True						# Print contour info				
 use_cuda = True								# Whether to use cuda
 test_sample_cuda = False
+blur = 'median'
+filter_contours = False
 
 # Load imports only if CUDA is enabled
 if use_cuda:
@@ -71,7 +73,10 @@ quant_end = time.clock()
 
 # Median step
 median_start = time.clock()
-blurred_image = cv2.medianBlur(quantized_image, median_kernel) 						# Remove noise with median kernel
+if blur == 'gaussian':
+	blurred_image = cv2.GaussianBlur(quantized_image, (5,5), 0) 						# Remove noise with median kernel
+else: 
+	blurred_image = cv2.medianBlur(quantized_image, median_kernel) 						# Remove noise with median kernel
 median_end = time.clock()
 
 # Edge Detection Step
@@ -81,40 +86,54 @@ edge_end = time.clock()
 
 # Contour Detection Step
 contour_image = blurred_image.copy()
-contour_start = time.clock()
-contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)	
+contour_start = time.clock()					# for best accuracy use binary images for contour detection
+contours, hierarchy = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)	# CHAIN_APPROX_NONE, RETR_TREE, RETR_LIST, RETR_FLOODFILL
+# CHAIN APPROX SIMPLE - saves memory
 contour_end = time.clock()	
 edges = cv2.cvtColor(edges, cv2.COLOR_GRAY2BGR)
 edges = cv2.bitwise_not(edges)
 cv2.drawContours(contour_image, contours, -1, (0, 255, 0), 2)
 
+# a contour can be 2 points
+
 # Filter Contours Step
 filter_contour_start = time.clock()
-num_shapes = len(contours)
-if filter_verbose:
-	print(num_shapes)
-new_contours = []
-for c in contours:
+if filter_contours:
+	num_shapes = len(contours)
 	if filter_verbose:
-		print(c.shape)
-	if perimeter_or_area_contour == 'perimeter':
-		contour_value = cv2.arcLength(c,True)
-	else:
-		area = cv2.contourArea(c)
-	if contour_value > contour_threshold:
-		new_contours.append(c)
+		print(num_shapes)
+	new_contours = []
+	for c in contours:
 		if filter_verbose:
-			print(contour_value)
+			print(c.shape)
+		if perimeter_or_area_contour == 'perimeter':
+			contour_value = cv2.arcLength(c,True)
+		else:
+			area = cv2.contourArea(c)
+		if contour_value > contour_threshold:
+			new_contours.append(c)
+			if filter_verbose:
+				print(contour_value)
+else:
+	new_contours = contours
 filter_contour_end = time.clock()
 filtered_image = blurred_image.copy()
-cv2.drawContours(filtered_image, new_contours, -1, (0, 255, 0), 2)
+cv2.drawContours(filtered_image, new_contours, -1, (0, 255, 0), 2) #2
 
 # Ideas: Convex Hull, Floodfill, draw bounding box, find median/reassign
 
+# Idea floodfill using seed point
+
+# Get final outline
+outline = cv2.bitwise_not(np.zeros((h,w,3), np.uint8))
+cv2.drawContours(outline, new_contours, -1, (0, 255, 0), 2) #2
+
+
 # Final Results
 row_1 = np.hstack([original_image, quantized_image, blurred_image])
-row_2 = np.hstack([edges, contour_image, filtered_image])
-images_to_show = np.vstack([row_1, row_2])
+row_2 = np.hstack([edges, contour_image, filtered_image]) 
+row_3 = np.hstack([outline, np.zeros((h,w,3), np.uint8), np.zeros((h,w,3), np.uint8)])
+images_to_show = np.vstack([row_1, row_2, row_3])
 cv2.imshow("Paint By Numbers", images_to_show)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
