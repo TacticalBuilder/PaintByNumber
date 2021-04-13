2e27dbbd79519920251fb7baa791ff1f1f6ca0bc
Kyle <kylebuettner0@gmail.com>
2021-04-13 12:37:00 -0400

    clean code

--- paint_by_numbers_pipeline.py
+++ paint_by_numbers_pipeline.py
@@ -7,28 +7,25 @@ from sklearn.datasets import load_sample_image
 import time
 import cv2
 import warnings
-
-# Options to Run Pipeline
-# What impacts runtime? Image shape, number of colors
 warnings.filterwarnings("ignore")
-image_name = 'test_images/soccer.jpg'			# Name of Image
+
+# Parameterizable Settings
+
+# Image Settings
+image_name = 'test_images/cat.jpg'			# Name of Image
 reshape_image = True						# Whether to reshape image dimensions
-reshape_width = 250 # 250
+reshape_width = 250 
 reshape_height = 250
 color_code = 1 								# Color code to read in (0 = grayscale, 1 = BGR)
-num_colors = 3							# Number of colors needed for k-means clustering
+num_colors = 5								# Number of colors needed for k-means clustering
 median_kernel = 5							# Size of median kernel used for blurring
-min_canny = 100								# Thresholds used for Canny edge detection
-max_canny = 200
-perimeter_or_area_contour = 'perimeter'		# How to filter out contours
-contour_threshold = 50						# Threshold to filter contours	
-filter_verbose = False						# Print contour info				
+blur = 'median'								# 'median' or 'gaussian'
+show_results = True							# Show plots?
+use_custom_rgb_to_lab = True				# Use custom RGB to LAB conversion function
+
+# GPU Settings
 use_cuda = True								# Whether to use cuda
-test_sample_cuda = False
-blur = 'median'
-filter_contours = False
-show_results = True
-use_custom_rgb_to_lab = True
+test_sample_cuda = False					# Test the sample cuda kernel
 
 # Load imports only if CUDA is enabled
 if use_cuda:
@@ -36,7 +33,7 @@ if use_cuda:
 	import pycuda.driver as drv
 	from pycuda.compiler import SourceModule
 
-
+# This is a lot of dummy data that I am in progress of testing on GPU
 # Run sample test case for Py CUDA
 if test_sample_cuda:
 	mod = SourceModule("""
@@ -48,7 +45,6 @@ if test_sample_cuda:
 	  }
 	}
 	""")
-	
 	multiply_them = mod.get_function("multiply_them")
 	a = np.random.randn(400).astype(np.float32)
 	b = np.random.randn(400).astype(np.float32)
@@ -65,14 +61,8 @@ if test_sample_cuda:
 	print(end - start)
 	print(dest-a*b)
 
-	
-
-#	#   constint row = blockIdx.x * blockDim.x + threadIdx.x;
-	#   int col = blockIdx.y * blockDim.y + threadIdx.y;
-# PIPELINE 
+# This is one of the cuda kernels; does not work yet but in process of making
 def convert_rgb_to_lab_gpu(img):
-
-	#multiply_them = mod.get_function("convert_rgb_to_lab_gpu")
 	mod = SourceModule("""
 	__global__ void pizza(float *dest, float *b, float *g, float *r)
 	{
@@ -84,24 +74,21 @@ def convert_rgb_to_lab_gpu(img):
 	b = img[:, :, 0].flatten()
 	g = img[:, :, 1].flatten()
 	r = img[:, :, 2].flatten()
-	print(b.shape)
 	dest = np.zeros_like(b)
 	pizza(drv.Out(dest), drv.In(b), drv.In(g), drv.In(r), block=(250,1,1), grid=(250,1,1))
 	print(dest)
-	exit()
 
-	pass
-
+# Custom convert RGB to LAB function
 def convert_rgb_to_lab(img):
 	assert img.dtype == 'uint8' # only handle this image
+	img = img.astype('float32') * (1.0/255.0) # Convert to fp b/w 0 and 1
 
-	# Convert image to fp and scale b/w 0 and 1
-	img = img.astype('float32') * (1.0/255.0)
-
+	# Gamma
 	func_srgb = lambda x : ((x+0.055)/1.055) ** (2.4) if x > 0.04045 else x / 12.92
 	vectorized_func_srgb = np.vectorize(func_srgb)
 	img = vectorized_func_srgb(img)
 
+	# Convert to XYZ and scale
 	transform_matrix = np.array([[0.412453, 0.357580, 0.180423], [0.212671, 0.715160, 0.072169], [0.019334, 0.119193, 0.950227]])
 	xyz = np.zeros(img.shape)
 	for i in range(img.shape[0]):
@@ -121,18 +108,15 @@ def convert_rgb_to_lab(img):
 	new_img[:, :, 1] = 500 * (vectorized_func_f_t(xyz[:,:,0]) - vectorized_func_f_t(xyz[:,:,1])) + delta
 	new_img[:, :, 2] = 200 * (vectorized_func_f_t(xyz[:,:,1]) - vectorized_func_f_t(xyz[:,:,2])) + delta
 
-
+	# Scale
 	new_img[:, :, 0] = (new_img[:, :, 0] * (255.0/100)).astype('uint8')
 	new_img[:, :, 1] = (new_img[:, :, 1] + 128).astype('uint8')
 	new_img[:, :, 2] = (new_img[:, :, 2] + 128).astype('uint8')
 
-
-	new_img[:, :, :] = new_img[:, :, :] 
-
 	return new_img
 
 
-
+# PIPELINE
 # Load Image
 test_image = cv2.imread(image_name, color_code)
 
@@ -147,139 +131,101 @@ original_image = test_image.copy()
 # Color Quantization
 quant_start = time.clock()
 
+## Convert RGB to LAB
 color_cvt1_start = time.clock()
 if use_custom_rgb_to_lab:
 	#convert_rgb_to_lab_gpu(test_image.copy())
 	quantized_image = convert_rgb_to_lab(test_image.copy())
-	#print(quantized_image[100][100])
 else:
-	quantized_image = cv2.cvtColor(test_image, cv2.COLOR_BGR2LAB)							# Convert from BGR to LAB
-	#print(quantized_image[100][100])
+	quantized_image = cv2.cvtColor(test_image, cv2.COLOR_BGR2LAB)							
 color_cvt1_end = time.clock()
 
+## Flatten h/w
 color_reshape1_start = time.clock()
-quantized_image = quantized_image.reshape((test_image.shape[0] * test_image.shape[1], 3))		# Flatten h/w dimensions
+quantized_image = quantized_image.reshape((test_image.shape[0] * test_image.shape[1], 3))		
 color_reshape1_end = time.clock()
 
+# Initialize k-means
 kmeans_start = time.clock()
-clusters = MiniBatchKMeans(n_clusters=num_colors)									# Run k-means
+clusters = MiniBatchKMeans(n_clusters=num_colors)									
 kmeans_end = time.clock()
 
+# Find clusters and assign labels to pixels (711, 1067, 3) -> (758637,)
 fit_predict_start = time.clock()
-labels = clusters.fit_predict(quantized_image)										# Finds clusters and assigns labels to pixels; (711, 1067, 3)->(758637,)
+labels = clusters.fit_predict(quantized_image)										
 fit_predict_end = time.clock()
 
+# Make quantized image
 assign_clusters_start = time.clock()
-quantized_image = clusters.cluster_centers_.astype("uint8")[labels]					# Make quantized image
+quantized_image = clusters.cluster_centers_.astype("uint8")[labels]				
 assign_clusters_end = time.clock()
 
+# Reshape quantized image
 color_reshape2_start = time.clock()
-quantized_image = quantized_image.reshape((h, w, 3))								# Reshape quantized image
+quantized_image = quantized_image.reshape((h, w, 3))								
 color_reshape2_end = time.clock()
 
+# Convert quantized image from LAB to BGR
 color_cvt2_start = time.clock()
-quantized_image = cv2.cvtColor(quantized_image, cv2.COLOR_LAB2BGR)					# Convert quantized image back to BGR
+quantized_image = cv2.cvtColor(quantized_image, cv2.COLOR_LAB2BGR)					
 color_cvt2_end = time.clock()
 
-quant_end = time.clock()
+quant_end = time.clock()		# End quantization
 
-# Median step
+# Blurring step
 median_start = time.clock()
 if blur == 'gaussian':
-	blurred_image = cv2.GaussianBlur(quantized_image, (5,5), 0) 						# Remove noise with median kernel
+	blurred_image = cv2.GaussianBlur(quantized_image, (5,5), 0) 						# Remove noise with gaussian kernel
 else: 
 	blurred_image = cv2.medianBlur(quantized_image, median_kernel) 						# Remove noise with median kernel
 median_end = time.clock()
 
-# Edge Detection Step
-edge_start = time.clock()
-edges = cv2.Canny(blurred_image, min_canny, max_canny)								# Get edges with Canny
-edge_end = time.clock()
-
-# Contour Detection Step
-contour_image = blurred_image.copy()
-contour_start = time.clock()					# for best accuracy use binary images for contour detection
-contours, hierarchy = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)	# CHAIN_APPROX_NONE, RETR_TREE, RETR_LIST, RETR_FLOODFILL
-# CHAIN APPROX SIMPLE - saves memory
-contour_end = time.clock()	
-orig_edges = edges.copy()
-edges = cv2.cvtColor(edges, cv2.COLOR_GRAY2BGR)
-edges = cv2.bitwise_not(edges)
-cv2.drawContours(contour_image, contours, -1, (0, 255, 0), 2)
-
-# Filter Contours Step
-filter_contour_start = time.clock()
-if filter_contours:
-	num_shapes = len(contours)
-	if filter_verbose:
-		print(num_shapes)
-	new_contours = []
-	for c in contours:
-		if filter_verbose:
-			print(c.shape)
-		if perimeter_or_area_contour == 'perimeter':
-			contour_value = cv2.arcLength(c,True)
-		else:
-			area = cv2.contourArea(c)
-		if contour_value > contour_threshold:
-			new_contours.append(c)
-			if filter_verbose:
-				print(contour_value)
-else:
-	new_contours = contours
-filter_contour_end = time.clock()
-filtered_image = blurred_image.copy()
-cv2.drawContours(filtered_image, new_contours, -1, (0, 255, 0), 2) #2
-
-# Get final outline
-outline = cv2.bitwise_not(np.zeros((h,w,3), np.uint8))
-cv2.drawContours(outline, new_contours, -1, (0, 255, 0), 2) #2
-
-# Need to make mask from each contour
-for c in new_contours:
-	mask = np.zeros((h,w,3),np.uint8)
-	cv2.drawContours(mask,new_contours, 0, 255, -1)
-
-# Crayola
-# Get filter out in this stage
-crayola_image = np.ones((h,w,3), np.uint8) * 255
+# Make Canvas - TO DO: Maybe filter in this stage?
+canvas_image = np.ones((h,w,3), np.uint8) * 255
 for i in range(w):
-	crayola_image[0][i] = [0, 0, 0]
-	crayola_image[h-1][i] = [0, 0, 0]
+	canvas_image[0][i] = [0, 0, 0]
+	canvas_image[h-1][i] = [0, 0, 0]
 for i in range(h):
-	crayola_image[i][0] = [0, 0, 0]
-	crayola_image[i][w-1] = [0, 0, 0]
+	canvas_image[i][0] = [0, 0, 0]
+	canvas_image[i][w-1] = [0, 0, 0]
+outline_image = canvas_image.copy()
 for i in range(1, h - 1):
 	for j in range(1, w - 1):
-		pixel_val = blurred_image[i][j]
-			#  1  2  3
-			#  4  X  5
-			#  6  7  8
+		pixel_val = blurred_image[i][j] # 123, 4X5, 678
 		if not np.array_equal(pixel_val, blurred_image[i-1][j-1]):
-			crayola_image[i,j] = [0, 0, 0]    
+			canvas_image[i,j] = [0, 0, 0]   
+			outline_image[i,j] = [0, 0, 0] 
 		elif not np.array_equal(pixel_val, blurred_image[i-1][j]):
-			crayola_image[i,j] = [0, 0, 0]
+			canvas_image[i,j] = [0, 0, 0]
+			outline_image[i,j] = [0, 0, 0] 
 		elif not np.array_equal(pixel_val, blurred_image[i-1][j+1]):
-			crayola_image[i,j] = [0, 0, 0]
+			canvas_image[i,j] = [0, 0, 0]
+			outline_image[i,j] = [0, 0, 0] 
 		elif not np.array_equal(pixel_val, blurred_image[i][j-1]):
-			crayola_image[i,j] = [0, 0, 0]
+			canvas_image[i,j] = [0, 0, 0]
+			outline_image[i,j] = [0, 0, 0] 
 		elif not np.array_equal(pixel_val, blurred_image[i][j+1]):
-			crayola_image[i,j] = [0, 0, 0]
+			canvas_image[i,j] = [0, 0, 0]
+			outline_image[i,j] = [0, 0, 0] 
 		elif not np.array_equal(pixel_val, blurred_image[i+1][j-1]):
-			crayola_image[i,j] = [0, 0, 0]
+			canvas_image[i,j] = [0, 0, 0]
+			outline_image[i,j] = [0, 0, 0] 
 		elif not np.array_equal(pixel_val, blurred_image[i+1][j]):
-			crayola_image[i,j] = [0, 0, 0] 
+			canvas_image[i,j] = [0, 0, 0] 
+			outline_image[i,j] = [0, 0, 0] 
 		elif not np.array_equal(pixel_val, blurred_image[i+1][j+1]):
-			crayola_image[i,j] = [0, 0, 0]
+			canvas_image[i,j] = [0, 0, 0]
+			outline_image[i,j] = [0, 0, 0] 
 		else: 
-			crayola_image[i, j] = blurred_image[i, j]
+			canvas_image[i, j] = blurred_image[i, j]
 
 # Get Connected Components 
-# Watch out if colored image
 marked_mask = np.zeros((h,w))
 dx = [-1, 0, 1, 1, 1, 0, -1, 1]
 dy = [1, 1, 1, 0, -1, -1, -1, 0]
 component_num = 1
+
+# DFS to assign regions
 def dfs(img, x_val, y_val, component_num):
 	stack = []
 	stack.append((x_val,y_val))
@@ -291,22 +237,23 @@ def dfs(img, x_val, y_val, component_num):
 			if (not np.array_equal(img[nx, ny], [0, 0, 0])) and marked_mask[nx, ny] == 0:
 				stack.insert(0, (nx, ny))
 				marked_mask[nx, ny] = component_num
-
+# Run DFS
 for i in range(1, h-1):
 	for j in range(1, w-1):
-		if (not np.array_equal(crayola_image[i, j], [0, 0, 0])) and marked_mask[i, j] == 0:
-			dfs(crayola_image, i, j, component_num)
+		if (not np.array_equal(canvas_image[i, j], [0, 0, 0])) and marked_mask[i, j] == 0:
+			dfs(canvas_image, i, j, component_num)
 			component_num = component_num + 1
-print(component_num)
-marked_mask = marked_mask.astype('uint8') * 20
+
+# Get num components
+print('Num of Components: ' + str(component_num))
+marked_mask = marked_mask.astype('uint8') * 20				# This is dummy line to show the 'marked_mask'; not robust
 marked_mask = cv2.cvtColor(marked_mask, cv2.COLOR_GRAY2BGR)
 
 # Final Results
 if show_results:
 	row_1 = np.hstack([original_image, quantized_image, blurred_image])
-	row_2 = np.hstack([edges, contour_image, filtered_image]) 
-	row_3 = np.hstack([outline, crayola_image, marked_mask])
-	images_to_show = np.vstack([row_1, row_2, row_3])
+	row_2 = np.hstack([outline_image, canvas_image, marked_mask])
+	images_to_show = np.vstack([row_1, row_2])
 	cv2.imshow("Paint By Numbers", images_to_show)
 	cv2.waitKey(0)
 	cv2.destroyAllWindows()
@@ -317,11 +264,8 @@ print('Color Quantization: ' + str(quant_end - quant_start) + ' s')
 print('\tFit Predict: ' + str(fit_predict_end - fit_predict_start) + ' s')
 print('\tReshape 1: ' + str(color_reshape1_end - color_reshape1_start) + ' s')
 print('\tConvert 1: ' + str(color_cvt1_end - color_cvt1_start) + ' s')
-print('\tK-Means: ' + str(kmeans_end - kmeans_start) + ' s')
+print('\tK-Means Init: ' + str(kmeans_end - kmeans_start) + ' s')
 print('\tAssign Clusters: ' + str(assign_clusters_end - assign_clusters_start) + ' s')
 print('\tReshape 2: ' + str(color_reshape2_end - color_reshape2_start) + ' s')
 print('\tConvert 2: ' + str(color_cvt2_end - color_cvt2_start) + ' s')
 print('Median Filter: ' + str(median_end - median_start) + ' s')
-print('Edge Detection: ' + str(edge_end - edge_start) + ' s')
-print('Contour: ' + str(contour_end - contour_start) + ' s')
-print('Filter Contour: ' + str(filter_contour_end - filter_contour_start) + ' s')
